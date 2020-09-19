903a90f5eccd7c07b37d4af60007fa04516198d5
nileshhazra <nileshhz99@gmail.com>
2020-09-19 12:38:44 +0530

    add linkedlist

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log("Hello JavaScript");
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  add(data) {
+    var node = new Node(data);
+
+    var current;
+
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  printlist() {
+    var current;
+    current = this.head;
+    while (current.head) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+var ll = new LinkedList();
+ll.add(10);
+ll.printlist();
