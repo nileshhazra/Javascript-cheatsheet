53573d8ce22e02cf165ab3665f455b197d587c8c
nileshhazra <nileshhz99@gmail.com>
2020-09-19 13:30:12 +0530

    add reduce function

---
+++ reduce.js
@@ -0,0 +1,7 @@
+var number = [3, 5, 7, 8, 10];
+
+var newNumber = number.reduce((accumulator, currentNumber) => accumulator + currentNumber)
+
+console.log("Final Sum = " + newNumber);
+// console.log("accumulator = " + accumulator);
+// console.log("currentNumber= " + currentNumber);
