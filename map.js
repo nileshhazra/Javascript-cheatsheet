3a6a63658a5bf9dc85dd9437e46acc71eea73754
nileshhazra <nileshhz99@gmail.com>
2020-09-20 19:48:51 +0530

    add loop in map function

--- map.js
+++ map.js
@@ -1,5 +1,11 @@
 var list = [1, 5, 7, 32, 55];
-
+var ll = [];
+// Using foreach loop
+list.forEach(x => {
+    ll.push(x * 2);
+});
+console.log(ll);;
+// Using map function 
 var newlist = list.map((x) => x * 2);
 
 console.log(newlist);
