d58eeb1fcef42429955b1628a46553043284f64a
nileshhazra <nileshhz99@gmail.com>
2020-09-20 18:37:13 +0530

    uncomment block

--- reduce.js
+++ reduce.js
@@ -1,11 +1,15 @@
 var number = [3, 5, 7, 8, 10];
+// JavaScript
+var newNum = number.reduce(function (accumulator, currentNumber) {
+  console.log("accumulator = " + accumulator);
+  console.log("currentNumber= " + currentNumber);
+  return accumulator + currentNumber;
+});
+console.log(newNum);
 
-// var newNumber = number.reduce(function (accumulator, currentNumber) {
-//     console.log("accumulator = " + accumulator);
-//     console.log("currentNumber= " + currentNumber);
-//     return accumulator + currentNumber;
-// })
-
-var newNumber = number.reduce((accumulator, currentNumber) => accumulator + currentNumber)
+//ES6 arrow function
+var newNumber = number.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
 
 console.log("Sum = " + newNumber);
