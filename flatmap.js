af30f4499d7a7f10bdb7e509729a95474f843cf1
nileshhazra <nileshhz99@gmail.com>
2020-09-20 19:37:03 +0530

    add foreach implementation

--- flatmap.js
+++ flatmap.js
@@ -1,5 +1,14 @@
 var numbers = [2, 4, 6];
 
+//Using foreach loop
+var new_num = [];
+numbers.forEach(x => {
+    new_num.push(x, x * 2)
+
+});
+
+console.log(new_num);
+//Using flatmap
 var newNumbers = numbers.flatMap(x => [x, x * 2])
 
 console.log(newNumbers);
