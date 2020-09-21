1cc59900be282bd00c36e18c92b6a7b9dcc76fea
nileshhazra <nileshhz99@gmail.com>
2020-09-21 20:33:54 +0530

    add semantic varibale name

--- flatmap.js
+++ flatmap.js
@@ -2,13 +2,12 @@ var numbers = [2, 4, 6];
 
 //Using foreach loop
 var new_num = [];
-numbers.forEach(x => {
-    new_num.push(x, x * 2)
-
+numbers.forEach((number) => {
+  new_num.push(number, number * 2);
 });
 
 console.log(new_num);
 //Using flatmap
-var newNumbers = numbers.flatMap(x => [x, x * 2])
+var newNumbers = numbers.flatMap((x) => [x, x * 2]);
 
 console.log(newNumbers);
