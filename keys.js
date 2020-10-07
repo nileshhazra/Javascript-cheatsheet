04605aafda1ed7b85fb68cec7cab4a5cfb7c79b8
nileshhazra <nileshhz99@gmail.com>
2020-10-07 20:23:49 +0530

    add keys function

---
+++ keys.js
@@ -0,0 +1,6 @@
+const arr1 = ['a', 'b', 'c', 'd'];
+const iterator = arr1.keys();
+
+for (const key of iterator) {
+    console.log(key);
+}
--- some.js
+++ some.js
@@ -1,3 +1,14 @@
 var numbers = [1, 2, 3, 4, 5];
 
-console.log(numbers.some(x => x % 2 === 0));
+console.log(numbers.some((x) => x % 2 === 0));
+
+function isEven(numbers) {
+  for (let index = 0; index < numbers.length; index++) {
+    if (numbers[index] % 2 === 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+isEven(numbers);
