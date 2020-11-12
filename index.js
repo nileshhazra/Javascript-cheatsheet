964dd6589c1eb35cd0cb53aef177561f4f7881d2
nileshhazra <nileshhz99@gmail.com>
2020-11-12 11:46:46 +0530

    factory and constructor function

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function() {
+// Factory Function
+function createCirlce(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    };
+  }
+
+const circle = createCirlce(1);
+circle.draw();
+
+// Constructor Function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
         console.log('draw');
     }
-};
-
-circle.draw();
+}
+const another = new Circle(1);
+another.draw();
