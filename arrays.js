a5c903a336d1d88683f49ea68c92c3503b4cd370
nileshhazra <nileshhz99@gmail.com>
2020-11-12 11:33:40 +0530

    add object literal

--- arrays.js
+++ arrays.js
@@ -4,8 +4,8 @@ var myVegies = ["Capsicum", "Onion", "Potato"];
 var myFood = myFruits.concat(myVegies);
 console.log(myFood);
 
-// myFood.splice(2, 0, "Lemon", "Chilli");
-// console.log(myFood);
+// myFood.splice(2, 0, "Lemon", "Chilli");  
+// console.log(myFood); 
 myFood.splice(2, 2, "Lemon", "Chilli");
 console.log(myFood);
 myFood.push('Brocolli')
